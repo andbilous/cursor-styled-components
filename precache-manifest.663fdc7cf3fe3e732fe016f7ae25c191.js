fd44838deaa0ca3e84277145da1594ecde21c90e
andbilous-digis <andbilous@gmail.com>
2020-04-19 22:10:51 +0300

    Updates

---
+++ precache-manifest.663fdc7cf3fe3e732fe016f7ae25c191.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d62d7a3b76a3e649ec572d229c55d24",
+    "url": "/cursor-styled-components/index.html"
+  },
+  {
+    "revision": "65f7196e5139961ca8ca",
+    "url": "/cursor-styled-components/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "c7f73906bd81291dd32a",
+    "url": "/cursor-styled-components/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "65f7196e5139961ca8ca",
+    "url": "/cursor-styled-components/static/js/2.0d9f1dc6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cursor-styled-components/static/js/2.0d9f1dc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7f73906bd81291dd32a",
+    "url": "/cursor-styled-components/static/js/main.5aab5663.chunk.js"
+  },
+  {
+    "revision": "129af9108cd3fcd1a964",
+    "url": "/cursor-styled-components/static/js/runtime-main.3a9633af.js"
+  }
+]);
